e53e0e3bb7bedeb542bf28e63fd92eeebe05a13f
Yuxiang Zhu <yz2329@cornell.edu>
2018-04-27 12:28:50 -0400

    Music!!

---
+++ bindings/python/samples/music-note.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from samplebase import SampleBase
+from rgbmatrix import graphics
+import time
+
+
+class MusicTest(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(MusicTest, self).__init__(*args, **kwargs)
+
+    def run(self):
+        canvas = self.matrix
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+
+        red = graphics.Color(255, 0, 0)
+        green = graphics.Color(0, 255, 0)
+        blue = graphics.Color(0, 0, 255)
+
+        graphics.DrawLine(canvas, 1, 1, 1, 5, red)
+
+        time.sleep(10)   # show display for 10 seconds before exit
+
+
+# Main function
+if __name__ == "__main__":
+    graphics_test = MusicTest()
+    if (not graphics_test.process()):
+        graphics_test.print_help()
