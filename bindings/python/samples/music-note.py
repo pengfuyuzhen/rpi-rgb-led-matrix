2ace1c0c4f6864327ad95766e1339dba4f6a220a
Yuxiang Zhu <yz2329@cornell.edu>
2018-04-27 12:57:48 -0400

    scroll implementation 2nd try

--- bindings/python/samples/music-note.py
+++ bindings/python/samples/music-note.py
@@ -21,12 +21,12 @@ class MusicTest(SampleBase):
 
         offset = 0
         while offset < 10:
-            canvas.SetPixel(0, offset, 0, 0)
+            canvas.SetPixel(0, offset, 0, 0, 0)
             offset += 1
             time.sleep(0.25)
 
 # Main function
 if __name__ == "__main__":
-    graphics_test = MusicTest()
-    if (not graphics_test.process()):
-        graphics_test.print_help()
+    music_test = MusicTest()
+    if (not music_test.process()):
+        music_test.print_help()
