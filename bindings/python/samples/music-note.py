faf438d65e658d268a8f745c14912d31ce7f22e5
Yuxiang Zhu <yz2329@cornell.edu>
2018-04-27 12:52:16 -0400

    scroll implementation

--- bindings/python/samples/music-note.py
+++ bindings/python/samples/music-note.py
@@ -10,19 +10,20 @@ class MusicTest(SampleBase):
 
     def run(self):
         canvas = self.matrix
-        font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
+        # font = graphics.Font()
+        # font.LoadFont("../../../fonts/7x13.bdf")
 
         red = graphics.Color(255, 0, 0)
         green = graphics.Color(0, 255, 0)
         blue = graphics.Color(0, 0, 255)
 
         graphics.DrawLine(canvas, 1, 1, 1, 5, red)
-        time.sleep(1)
-        graphics.DrawLine(canvas, 1, 2, 1, 6, red)
-        time.sleep(1)
-        graphics.DrawLine(canvas, 1, 3, 1, 7, red)
-        time.sleep(1)
+
+        offset = 0
+        while offset < 10:
+            canvas.SetPixel(0, offset, 0, 0)
+            offset += 1
+            time.sleep(0.25)
 
 # Main function
 if __name__ == "__main__":
